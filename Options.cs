917170dc4fa641f5f773d49c5cc49f67c56f2667
anefedyev <anefedyev@mail.ru>
2024-11-08 23:05:00 +0300

    Добавьте файлы проекта.

---
+++ Options.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace TranslatorClient
+{
+    public class Options
+    {
+        [Option('d', "definition", Required = true, HelpText = "Input a text for translate")]
+        public string InputText { get; set; }
+
+        [Option('s', "sourceLanguage", Required = true, HelpText = "The source language.", Default = "ru")]
+        public string SourceLanguage { get; set; }
+
+        [Option('t', "targetLanguage", Required = true, HelpText = "The target language.", Default = "en")]
+        public string TargetLanguage { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Net.Client;
+using ExampleSpace;
+using Microsoft.Extensions.Configuration;
+using TranslatorClient;
+using CommandLine;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await Parser.Default.ParseArguments<Options>(args)
+          .WithParsedAsync(RunOptionsAndReturnExitCode);
+    }
+
+    private async static Task RunOptionsAndReturnExitCode(Options opts)
+    {
+        var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())       
+                       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+        var channel = GrpcChannel.ForAddress(configuration["freeUrl"] ?? "https://localhost:5000");
+        var client = new Translater.TranslaterClient(channel);
+        var request = new TranslateRequest
+        {
+            SourceLang = opts.SourceLanguage,
+            TargetLang = opts.TargetLanguage,
+            TranslateText = opts.InputText
+        };
+
+        TranslateResponse response = await client.DoTranslateAsync(request);
+
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Translated text: {response.TranslateResult}");
+        Console.ResetColor();
+
+        await channel.ShutdownAsync();
+    }
+}
